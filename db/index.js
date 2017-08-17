88b4b1bcf5f8a1e091e2876a56528679c8cb3d9b
Cameron Epstein <cameron.a.epstein@gmail.com>
2017-08-17 10:31:29 +0100

    connect to database

--- db/index.js
+++ db/index.js
@@ -1,12 +1,22 @@
 'use strict'
 
 const { Pool } = require('pg');
-const { config } = require('../config.js');
+const config = require('../config');
 
 const pool = new Pool({
-  user: '',
-  host:
+  user: config.keys.username,
+  database: config.keys.database,
+  password: config.keys.password,
+  host: config.keys.host,
+  post: config.keys.port,
+  max: 10,
+  idleTimeoutMillis: 3000,
 });
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+})
 
 module.exports = {
   query: (text, params, callback) => {
